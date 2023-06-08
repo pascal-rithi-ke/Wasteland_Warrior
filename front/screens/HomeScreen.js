445d403dfad56a7bbee17f9a045738a66dee90d4
Malo <malo.serot@etudiant.univ-reims.fr>
2023-06-09 00:29:38 +0200

    Style page d'accueil

--- front/screens/HomeScreen.js
+++ front/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 
@@ -8,37 +8,63 @@ function HomeScreen() {
 
   return (
     <View style={styles.containerStart}>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('Synopsis')}>Joueur</Text>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('ContinueGame')}>Reprendre la partie</Text>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('Regles')}>Règles</Text>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('Create_User')}>S'inscrire</Text>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('Delete_User')}>Supprimer le compte</Text>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('Login')}>Se connecter</Text>
-      <Text>-------- PASCAL --------</Text>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('Add_Histoire')}>Add_Histoire</Text>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('Update_Histoire')}>Update_Histoire</Text>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('Delete_Histoire')}>Delete_Histoire</Text>
-      <Text style={styles.btnStart} onPress={() => navigation.navigate('GetAllHistoire')}>GetAllHistoire</Text>
-      <Text>------------------------</Text>
+      <ImageBackground source={require('../assets/fond.png')} resizeMode="cover" style={styles.background}>
+        <Text style={styles.title}>Wasteland Warrior</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('Synopsis')}>Jouer</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('ContinueGame')}>Reprendre la partie</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('Regles')}>Règles</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('Create_User')}>S'inscrire</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('Delete_User')}>Supprimer le compte</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('Login')}>Se connecter</Text>
+        <Text>-------- PASCAL --------</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('Add_Histoire')}>Add_Histoire</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('Update_Histoire')}>Update_Histoire</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('Delete_Histoire')}>Delete_Histoire</Text>
+        <Text style={styles.btnStart} onPress={() => navigation.navigate('GetAllHistoire')}>GetAllHistoire</Text>
+        <Text>------------------------</Text>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    resizeMode: 'center',
+    justifyContent: 'center',
+    padding: 16,
+    alignSelf: 'stretch',
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: 'bold',
+    color: '#14f819',
+    marginBottom: 10,
+    fontFamily: 'monospace',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
   containerStart: {
     alignItems: 'center',
     backgroundColor: '#fff',
     flex: 1,
     justifyContent: 'center',
+    padding: 0,
+    alignSelf: 'stretch',
   },
   btnStart: {
-    backgroundColor: '#000',
+    backgroundColor: '#0e8210',
     borderRadius: 30,
     color: '#FFFFFF',
+    borderColor: '#14f819',
+    borderWidth: 3,
     fontSize: 20,
     fontWeight: 'bold',
     padding: 15,
     textAlign: 'center',
+    fontFamily: 'monospace',
     width: 250,
     marginBottom: 20,
   },
