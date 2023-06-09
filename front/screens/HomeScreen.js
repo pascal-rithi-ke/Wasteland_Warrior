c6de98d424358fe868ca54aba2395c514fd68566
pascal-rithi-ke <pascal.rithi@gmail.com>
2023-06-09 09:09:19 +0200

    touchableOpacity btn

--- front/screens/HomeScreen.js
+++ front/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, Image, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 
@@ -10,17 +10,30 @@ function HomeScreen() {
     <View style={styles.containerStart}>
       <ImageBackground source={require('../assets/fond.png')} resizeMode="cover" style={styles.background}>
         <Text style={styles.title}>Wasteland Warrior</Text>
-        <Text style={styles.btnStart} onPress={() => navigation.navigate('Synopsis')}>Jouer</Text>
-        <Text style={styles.btnStart} onPress={() => navigation.navigate('ContinueGame')}>Reprendre la partie</Text>
-
-        <Text style={styles.btnStart} onPress={() => navigation.navigate('Règles')}>Règles</Text>
-        
-        <Text style={styles.btnStart} onPress={() => navigation.navigate('Inscription')}>S'inscrire</Text>
-        <Text style={styles.btnStart} onPress={() => navigation.navigate('Suppression Utilisateur')}>Supprimer le compte</Text>
-        <Text style={styles.btnStart} onPress={() => navigation.navigate('Connexion')}>Se connecter</Text>
-
-        <Text style={styles.btnStart} onPress={() => navigation.navigate('Ajouter histoire')}>Ajouter Histoire</Text>
-        <Text style={styles.btnStart} onPress={() => navigation.navigate('Toutes les histoires')}>Toutes les histoires</Text>
+        <TouchableOpacity style={styles.btnStart} onPress={() => navigation.navigate('Synopsis')}>
+          <Text style={styles.text}>Jouer</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnStart} onPress={() => navigation.navigate('ContinueGame')}>
+          <Text style={styles.text}>Reprendre la partie</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnStart} onPress={() => navigation.navigate('Règles')}>
+          <Text style={styles.text}>Règles</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnStart} onPress={() => navigation.navigate('Inscription')}>
+          <Text style={styles.text}>S'inscrire</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnStart} onPress={() => navigation.navigate('Suppression Utilisateur')}>
+          <Text style={styles.text}>Supprimer le compte</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnStart} onPress={() => navigation.navigate('Connexion')}>
+          <Text style={styles.text}>Se connecter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnStart} onPress={() => navigation.navigate('Ajouter histoire')}>
+          <Text style={styles.text}>Ajouter Histoire</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnStart} onPress={() => navigation.navigate('Toutes les histoires')}>
+          <Text style={styles.text}>Toutes les histoires</Text>
+        </TouchableOpacity>
       </ImageBackground>
     </View>
   );
@@ -56,16 +69,22 @@ const styles = StyleSheet.create({
   btnStart: {
     backgroundColor: '#0e8210',
     borderRadius: 30,
-    color: '#FFFFFF',
     borderColor: '#14f819',
     borderWidth: 3,
-    fontSize: 9,
     fontWeight: 'bold',
     padding: 15,
     textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
     fontFamily: 'monospace',
     width: 250,
     marginBottom: 20,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 9,
+    fontWeight: 'bold',
+    fontFamily: 'monospace',
   },
 });
 
